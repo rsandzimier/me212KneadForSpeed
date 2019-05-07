5572663d645d504b887b310875c0abe67fb61edf
Arlette <arlette@mit.edu>
2019-05-07 15:48:24 -0400

    Added depth filter to waiter detection.

--- src/mobile_robot/src/waiter_detection.py
+++ src/mobile_robot/src/waiter_detection.py
@@ -14,13 +14,14 @@ class CV:
 		self.rate = 100 #[Hz]
 
 		self.bridge = CvBridge()
-		self.image_dims = []
+		self.image_dims = None
 		self.bob = None
 		self.once = False
-		self.depth_bob = None
+
+		self.rgb_image = None
+		self.depth_image = None
 
 		rospy.Subscriber("/camera/rgb/image_rect_color/", Image, self.image_cb) # Need to use rectified image instead
-
 
 		rospy.Subscriber("/camera/depth_registered/image", Image, self.depth_cb)
 
@@ -29,35 +30,31 @@ class CV:
 
 
 	def depth_cb(self, msg):
-		if self.bob is None:
-			return
 		try:
 			cv_depthimage = self.bridge.imgmsg_to_cv2(msg, "32FC1")
-			cv_depthimage2 = np.array(cv_depthimage, dtype=np.float32)
+			self.depth_image = np.array(cv_depthimage, dtype=np.float32)
 		except CvBridgeError as e:
 			print(e)
-
-		if not self.once:
-			print np.min(cv_depthimage2[np.logical_not(np.isnan(cv_depthimage2))])
-			filtered_bob = self.filter_depth(self.bob, 1.0, 0.0, cv_depthimage2)
-			# self.once = True
-			print np.unique(filtered_bob)
-			self.depth_bob = filtered_bob
-				
-
 
 	def image_cb(self, msg):
 		try:
-			cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+			self.rgb_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+			if self.image_dims is None:
+				self.image_dims = self.rgb_image.shape
 		except CvBridgeError as e:
 			print(e)
-		if len(self.image_dims) == 0:
-			self.image_dims = cv_image.shape
-		cv_image = self.adjust_gamma(cv_image, 1)
+		# cv2.imshow("Image Window", self.rgb_image)
+		# cv2.waitKey(3)
+		self.process()
 
-		if self.depth_bob is not None:
-			cv2.imshow("depth_bob", self.depth_bob)
-			cv2.waitKey(3)
+	def process(self):
+		if self.rgb_image == None or self.depth_image == None: 
+			return
+		depth_mask = self.filter_depth(self.depth_image, 1.0, 0.0)
+
+		cv_image = cv2.bitwise_and(self.rgb_image,self.rgb_image,mask = depth_mask)
+
+		cv_image = self.adjust_gamma(cv_image, 1)
 
 		cv2.imshow("Image Window", cv_image)
 		cv2.waitKey(3)
@@ -77,11 +74,11 @@ class CV:
 		#glass = self.hsv_filter(cv_image, [100,0,100], [150,40,175], True) # Glass too close to white color. Maybe don't use it
 
 		# Noise reduction
-		bow_tie = self.noise_reduction(bow_tie,10,1)
-		hair = self.noise_reduction(hair,5,1)
-		pants = self.noise_reduction(pants,5,1)
-		skin = self.noise_reduction(skin,5,1)
-		tray = self.noise_reduction(tray,5,1)
+		bow_tie = self.noise_reduction(bow_tie,3,1)
+		hair = self.noise_reduction(hair,3,1)
+		pants = self.noise_reduction(pants,3,1)
+		#skin = self.noise_reduction(skin,1,1)
+		#tray = self.noise_reduction(tray,1,1)
 
 
 		cv2.imshow("bow", bow_tie)
@@ -96,10 +93,10 @@ class CV:
 		cv2.waitKey(3)
 
 		merged_bob = bow_tie
-		for p in [hair, pants, skin, tray]:
+		for p in [hair, pants]:
 			merged_bob = cv2.bitwise_or(p, merged_bob)
 
-		merged_bob_reduced = self.noise_reduction(merged_bob,5,1)
+		merged_bob_reduced = self.noise_reduction(merged_bob,3,1)
 		
 		self.bob = merged_bob_reduced
 			
@@ -171,8 +168,10 @@ class CV:
 		# slots_msg = DetectionArray()
 		# slots_msg.detections = slot_detections
 		# self.slots_pub.publish(slots_msg)
+		self.rgb_image = None
+		self.depth_image = None
 
-	def filter_depth(self, image, upper_depth, lower_depth, depth_image):
+	def filter_depth(self, depth_image, upper_depth, lower_depth):
 		# image is a np.array
 		#depth_mask is a np.array
 		#upper_depth, lower_Depth, float
@@ -186,28 +185,10 @@ class CV:
 		depth_image[(np.isnan(depth_image)==True)] = upper_depth+1
 		#ixs = np.where()
 		ixs = []
-		print np.min(depth_image), np.max(depth_image)
 		# print ((lower_depth<depth_image)&(depth_image<upper_depth))
 		depth_mask[(lower_depth<depth_image)&(depth_image<upper_depth)] = 255
-		print np.unique((lower_depth<depth_image)&(depth_image<upper_depth)), np.unique(image)
 		depth_mask = depth_mask.astype('uint8')
-		# print image
-		filtered_image = cv2.bitwise_and(depth_mask, image)
-		return filtered_image
-
-
-	def imgPose2DetectionMsg(self, img_pose, detection_type):
-		# Takes in an image pose (x,y in pixels and orientation in radians) and outputs a detection message (x,y,z in mm, orientation in radians, and detection type)
-		# Type: 0-pepperoni, 1- olive, 2-ham, 3-pineapple, 4-anchovie, -1-open slot
-		d = Detection()
-		# Populate d with data x,y,z, and orientation are in cartesian space relative to the delta robot origin/coordinate system
-		d.type = detection_type
-		#d.position.x = 
-		#d.position.y = 
-		#d.position.z = 
-		#d.orientation = 
-		pass
-		return d
+		return depth_mask
 
 	def adjust_gamma(self, image, gamma=1.0):
 	   table = np.array([((i / 255.0) ** (1.0 / gamma)) * 255 for i in np.arange(0, 256)]).astype("uint8")
@@ -234,34 +215,6 @@ class CV:
 		kernel = np.ones((kernel_size,kernel_size),np.uint8)
 		erosion = cv2.erode(image,kernel,iterations = iterations)
 		return cv2.dilate(erosion,kernel,iterations = iterations)
-
-
-	def merge_blobs(self, image1, image2, minArea1, minArea2, minDist, display=False):
-		# image must be binary
-		# Find contours in binary image and ignore any with area less than minArea
-		im1, contours1, hierarchy1 = cv2.findContours(image1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-		im2, contours2, hierarchy2 = cv2.findContours(image2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-		contours1 = [c for c in contours1 if cv2.contourArea(c) >= minArea1]
-		contours2 = [c for c in contours2 if cv2.contourArea(c) >= minArea2]
-
-		kernel = np.ones((minDist/2,minDist/2))
-		print len(contours1),",",len(contours2)
-		merged = []
-		for c1 in contours1:
-			for c2 in contours2:
-				dilated = cv2.bitwise_or(self.fillContoursBinary([c1]), self.fillContoursBinary([c2]))
-				dilated = cv2.dilate(dilated,kernel,iterations = 1)
-				im2, contours_dilated, hierarchy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-				if len(contours_dilated) == 1:
-					merged.append(contours_dilated[0])
-
-		print len(merged)
-		merged_img = cv2.erode(self.fillContoursBinary(merged),kernel,iterations = 1)
-
-		if display:
-			cv2.imshow("Merged", merged_img)
-			cv2.waitKey(3)
-		return merged_img
 
 
 	def fillContoursBinary(self, contours):
