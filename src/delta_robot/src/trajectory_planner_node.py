Tested and tuned traj_follower dead reckoning trajectories. Fixed a minor bug in the follower node related to the gripper rotation. Completed the publishing part of the interface node.

src/delta_robot/src/trajectory_planner_node.py
@@ -16,12 +16,14 @@ import numpy as np
 
 class TrajectoryPlanner: 
 	XYZ_VEL = 100.0
-    XYZ_ACCEL = 4900.0
-    XYZ_VEL_FAST = 100.0
-    XYZ_ACCEL_FAST = 4900.0
-    pizzaradius=115.0
-    shake_distance=10.0
-	ZOFFSET = 50.0
+	XYZ_ACCEL = 4900.0
+	XYZ_VEL_FAST = 100.0
+	XYZ_ACCEL_FAST = 4900.0
+	pizzaradius=130.0
+	shake_distance=10.0
+	ZOFFSET = 40.0
+	MAX_PRESS_RADIUS = 85
+	NUM_SHAKES = 6
 
 	def __init__(self): 
 		self.rate = 100.0 #[Hz]
@@ -112,22 +114,22 @@ class TrajectoryPlanner:
 		if self.running_task:
 			print "Already working on a task"
 			return
-        self.running_task = True
-        shaker_xyz=[0,0,0]
+		self.running_task = True
+		shaker_xyz=[0,0,0]
 		shaker_orientation=0
 		shaker_xyz[0]=msg.poses[0].position.x
 		shaker_xyz[1]=msg.poses[0].position.y
 		shaker_xyz[2]=msg.poses[0].position.z
-        shaker_orientation=msg.poses[0].orientation
-        
-        above_shaker_xyz=[0,0,0]
+		shaker_orientation=msg.poses[0].orientation
+		
+		above_shaker_xyz=[0,0,0]
 		above_shaker_orientation=0
 		above_shaker_xyz[0]=msg.poses[0].position.x
 		above_shaker_xyz[1]=msg.poses[0].position.y
 		above_shaker_xyz[2]=msg.poses[0].position.z+self.ZOFFSET
-        above_shaker_orientation=msg.poses[0].orientation
-        
-        dest_xyz=[0,0,0]
+		above_shaker_orientation=msg.poses[0].orientation
+		
+		dest_xyz=[0,0,0]
 		dest_orientation=0
 		dest_xyz[0]=msg.poses[1].position.x
 		dest_xyz[1]=msg.poses[1].position.y
@@ -139,11 +141,11 @@ class TrajectoryPlanner:
 		above_dest_xyz[0]=msg.poses[1].position.x
 		above_dest_xyz[1]=msg.poses[1].position.y
 		above_dest_xyz[2]=msg.poses[1].position.z+self.ZOFFSET
-        above_dest_orientation=msg.poses[1].orientation
-        
-        
+		above_dest_orientation=msg.poses[1].orientation
+		
+		
 
-        above_dest_left_xyz=[0,0,0]
+		above_dest_left_xyz=[0,0,0]
 		above_dest_orientation=0
 		above_dest_left_xyz[0]=msg.poses[1].position.x-self.shake_distance
 		above_dest_left_xyz[1]=msg.poses[1].position.y
@@ -156,147 +158,158 @@ class TrajectoryPlanner:
 		above_dest_right_xyz[1]=msg.poses[1].position.y
 		above_dest_right_xyz[2]=msg.poses[1].position.z+self.ZOFFSET
 		above_dest_right_orientation=msg.poses[1].orientation
-        
-        self.generateMoveTo(above_shaker_xyz,above_shaker_orientation,True)
+		
+		self.generateMoveTo(above_shaker_xyz,above_shaker_orientation,True)
 		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(shaker_xyz,shaker_orientation,True)
 		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(shaker_xyz,shaker_orientation,False)
 		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(above_shaker_xyz,above_shaker_orientation,False)
-        self.waitForTrajectoryToFinish()
-        print('grabbed shaker')
-        
+		self.waitForTrajectoryToFinish()
+		print('grabbed shaker')
+		
 		self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
-        self.waitForTrajectoryToFinish()
-        
-		self.generateMoveToFAST(above_dest_left_xyz, dest_orientation, False)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveToFAST(above_dest_right_xyz, dest_orientation, False)
-        self.waitForTrajectoryToFinish()
-        print('im shook for the first time')
-        self.generateMoveToFAST(above_dest_left_xyz, dest_orientation, False)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveToFAST(above_dest_right_xyz, dest_orientation, False)
-        self.waitForTrajectoryToFinish()
-        print('im shook for the second time')
+		self.waitForTrajectoryToFinish()
+		i=0
+		for i in range(self.NUM_SHAKES):
+			self.generateMoveToFAST(above_dest_left_xyz, dest_orientation, False)
+			self.waitForTrajectoryToFinish()
+			self.generateMoveToFAST(above_dest_right_xyz, dest_orientation, False)
+			self.waitForTrajectoryToFinish()
+			print('im shook for the '+str(i)+' time')
+		'''self.generateMoveToFAST(above_dest_left_xyz, dest_orientation, False)
+		self.waitForTrajectoryToFinish()
+		self.generateMoveToFAST(above_dest_right_xyz, dest_orientation, False)
+		self.waitForTrajectoryToFinish()
+		print('im shook for the second time')'''
 
-        self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
-        self.waitForTrajectoryToFinish()
-        
-        self.generateMoveTo(above_shaker_xyz,above_shaker_orientation,False)
+		self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
+		self.waitForTrajectoryToFinish()
+		
+		self.generateMoveTo(above_shaker_xyz,above_shaker_orientation,False)
 		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(shaker_xyz,shaker_orientation,False)
 		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(shaker_xyz,shaker_orientation,True)
 		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(above_shaker_xyz,above_shaker_orientation,True)
-        self.waitForTrajectoryToFinish()
-        
-        print("Shook salt!")
+		self.waitForTrajectoryToFinish()
+		
+		print("Shook salt!")
 		self.running_task = False
 		finished_task_msg = Bool()
 		finished_task_msg.data = True
 		self.finished_task_pub.publish(finished_task_msg)
-        
+		
 
 	def press_dough_cb(self,msg): 
-        if self.running_task:
-            print "Already working on a task"
-            return
-        self.running_task = True
-        presser_xyz=[0,0,0]
-        presser_orientation=0
-        presser_xyz[0]=msg.poses[0].position.x
-        presser_xyz[1]=msg.poses[0].position.y
-        presser_xyz[2]=msg.poses[0].position.z
-        presser_orientation=msg.poses[0].orientation
+		if self.running_task:
+			print "Already working on a task"
+			return
+		self.running_task = True
+		presser_xyz=[0,0,0]
+		presser_orientation=0
+		presser_xyz[0]=msg.poses[0].position.x
+		presser_xyz[1]=msg.poses[0].position.y
+		presser_xyz[2]=msg.poses[0].position.z
+		presser_orientation=msg.poses[0].orientation
 
-        above_presser_xyz=[0,0,0]
-        above_presser_orientation=0
-        above_presser_xyz[0]=msg.poses[0].position.x
-        above_presser_xyz[1]=msg.poses[0].position.y
-        above_presser_xyz[2]=msg.poses[0].position.z+self.ZOFFSET
-        above_presser_orientation=msg.poses[0].orientation
+		above_presser_xyz=[0,0,0]
+		above_presser_orientation=0
+		above_presser_xyz[0]=msg.poses[0].position.x
+		above_presser_xyz[1]=msg.poses[0].position.y
+		above_presser_xyz[2]=msg.poses[0].position.z+self.ZOFFSET
+		above_presser_orientation=msg.poses[0].orientation
 
-        dest_xyz=[0,0,0]
-        dest_orientation=0
-        dest_xyz[0]=msg.poses[1].position.x
-        dest_xyz[1]=msg.poses[1].position.y
-        dest_xyz[2]=msg.poses[1].position.z
-        dest_orientation=msg.poses[1].orientation
+		dest_xyz=[0,0,0]
+		dest_orientation=0
+		dest_xyz[0]=msg.poses[1].position.x
+		dest_xyz[1]=msg.poses[1].position.y
+		dest_xyz[2]=msg.poses[1].position.z
+		dest_orientation=msg.poses[1].orientation
 
-        above_dest_xyz=[0,0,0]
-        above_dest_orientation=0
-        above_dest_xyz[0]=msg.poses[1].position.x
-        above_dest_xyz[1]=msg.poses[1].position.y
-        above_dest_xyz[2]=msg.poses[1].position.z+self.ZOFFSET
-        above_dest_orientation=msg.poses[1].orientation
+		above_dest_xyz=[0,0,0]
+		above_dest_orientation=0
+		above_dest_xyz[0]=msg.poses[1].position.x
+		above_dest_xyz[1]=msg.poses[1].position.y
+		above_dest_xyz[2]=msg.poses[1].position.z+self.ZOFFSET
+		above_dest_orientation=msg.poses[1].orientation
 
 
-        self.generateMoveTo(above_presser_xyz,above_presser_orientation,True)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveTo(presser_xyz,presser_orientation,True)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveTo(presser_xyz,presser_orientation,False)
-        self.waitForTrajectoryToFinish()
-        print('grabbed presser')
-        self.generateMoveTo(above_presser_xyz,above_presser_orientation,False)
-        self.waitForTrajectoryToFinish()
-        theta=0.0
-        while theta<360.0:
-            r=self.pizzaradius*theta/360.0
-            x_off=r*math.cos(math.radians(theta))
-            y_off=r*math.sin(math.radians(theta))
-            print([x_off,y_off,theta])
-            dest_xyz[0]=msg.poses[1].position.x+x_off
-            dest_xyz[1]=msg.poses[1].position.y+y_off
-            dest_xyz[2]=msg.poses[1].position.z
-            above_dest_xyz[0]=msg.poses[1].position.x+x_off
-            above_dest_xyz[1]=msg.poses[1].position.y+y_off
-            above_dest_xyz[2]=msg.poses[1].position.z+self.ZOFFSET
-            self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
-            self.waitForTrajectoryToFinish()
-            self.generateMoveTo(dest_xyz, dest_orientation, False)
-            self.waitForTrajectoryToFinish()
-            self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
-            self.waitForTrajectoryToFinish()
-            theta+=60.0
+		self.generateMoveTo(above_presser_xyz,above_presser_orientation,True)
+		self.waitForTrajectoryToFinish()
+		self.generateMoveTo(presser_xyz,presser_orientation,True)
+		self.waitForTrajectoryToFinish()
+		self.generateMoveTo(presser_xyz,presser_orientation,False)
+		self.waitForTrajectoryToFinish()
+		print('grabbed presser')
+		self.generateMoveTo(above_presser_xyz,above_presser_orientation,False)
+		self.waitForTrajectoryToFinish()
+		r = 40.0
+		while (r < self.MAX_PRESS_RADIUS):
+			theta=0.0
+			while theta<360.0:
+				#r=self.pizzaradius*theta/360.0
+				x_off=r*math.cos(math.radians(theta))
+				y_off=r*math.sin(math.radians(theta))
+				print([x_off,y_off,theta])
+				dest_xyz[0]=msg.poses[1].position.x+x_off
+				dest_xyz[1]=msg.poses[1].position.y+y_off
+				dest_xyz[2]=msg.poses[1].position.z
+				above_dest_xyz[0]=msg.poses[1].position.x+x_off
+				above_dest_xyz[1]=msg.poses[1].position.y+y_off
+				above_dest_xyz[2]=msg.poses[1].position.z+self.ZOFFSET
+				self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
+				self.waitForTrajectoryToFinish()
+				self.generateMoveTo(dest_xyz, dest_orientation, False)
+				self.waitForTrajectoryToFinish()
+				self.generateMoveTo(above_dest_xyz, above_dest_orientation, False)
+				self.waitForTrajectoryToFinish()
+				theta+=30.0
+			r = r + 40.0
 
-        self.generateMoveTo(above_presser_xyz, above_presser_orientation, False)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveTo(presser_xyz,presser_orientation,False)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveTo(presser_xyz,presser_orientation,True)
-        self.waitForTrajectoryToFinish()
-        self.generateMoveTo(above_presser_xyz,above_presser_orientation,True)
-        self.waitForTrajectoryToFinish()
+		self.generateMoveTo(above_presser_xyz, above_presser_orientation, False)
+		self.waitForTrajectoryToFinish()
+		self.generateMoveTo(presser_xyz,presser_orientation,False)
+		self.waitForTrajectoryToFinish()
+		self.generateMoveTo(presser_xyz,presser_orientation,True)
+		self.waitForTrajectoryToFinish()
+		self.generateMoveTo(above_presser_xyz,above_presser_orientation,True)
+		self.waitForTrajectoryToFinish()
 
-        print("Pressed Dough!")
-        self.running_task = False
-        finished_task_msg = Bool()
-        finished_task_msg.data = True
-        self.finished_task_pub.publish(finished_task_msg)
-        
+		print("Pressed Dough!")
+		self.running_task = False
+		finished_task_msg = Bool()
+		finished_task_msg.data = True
+		self.finished_task_pub.publish(finished_task_msg)
+		
 
 	def push_pizza_cb(self,msg):
 		if self.running_task:
 			print("Already working on a task")
 			return
 		self.running_task = True
-        
+		
 		dest_xyz=[0,0,0]
 		dest_orientation=0
 		dest_xyz[0]=msg.position.x-self.pizzaradius
 		dest_xyz[1]=msg.position.y
 		dest_xyz[2]=msg.position.z
 		dest_orientation=msg.orientation
+
+		above_dest_xyz = [0,0,0]
+		above_dest_xyz[0]=msg.position.x-self.pizzaradius
+		above_dest_xyz[1]=msg.position.y
+		above_dest_xyz[2]=msg.position.z+self.ZOFFSET
+		self.generateMoveTo(above_dest_xyz, 0, False)
+		self.waitForTrajectoryToFinish()
 		self.generateMoveTo(dest_xyz,dest_orientation,False)
 		self.waitForTrajectoryToFinish()
-		dest_xyz[0] = dest_xyz[0]+3*self.pizzaradius
+		dest_xyz[0] = dest_xyz[0]+2.5*self.pizzaradius
 		self.generateMoveTo(dest_xyz,dest_orientation,False)
-        self.waitForTrajectoryToFinish()
-        print("Pushed Pizza!")
+		self.waitForTrajectoryToFinish()
+		print("Pushed Pizza!")
 		self.running_task = False
 		finished_task_msg = Bool()
 		finished_task_msg.data = True
@@ -320,82 +333,82 @@ class TrajectoryPlanner:
 		self.running_task = False
 		finished_task_msg = Bool()
 		finished_task_msg.data = True
-        self.finished_task_pub.publish(finished_task_msg)
-        
-    def generateMoveToFAST(self,xyz,orientation,gripper_open):
-        if self.running_trajectory:
-        print("Currently running a trajectory. Cannot call this function until goal position reached")
-        return
+		self.finished_task_pub.publish(finished_task_msg)
+		
+	def generateMoveToFAST(self,xyz,orientation,gripper_open):
+		if self.running_trajectory:
+			print("Currently running a trajectory. Cannot call this function until goal position reached")
+			return
 
-        self.running_task = True
-        desired_xyz = np.array(xyz)
-        desired_rot = orientation
-        desired_open = gripper_open
+		self.running_task = True
+		desired_xyz = np.array(xyz)
+		desired_rot = orientation
+		desired_open = gripper_open
 
-        xyz_initial = np.array(self.xyz_pos)
-        diff = desired_xyz - xyz_initial
-        d = np.linalg.norm(diff)
+		xyz_initial = np.array(self.xyz_pos)
+		diff = desired_xyz - xyz_initial
+		d = np.linalg.norm(diff)
 
-        tr = self.XYZ_VEL_FAST/self.XYZ_ACCEL_FAST
-        tm = d/self.XYZ_VEL_FAST - tr
-        if tm < 0:
-            tr = math.sqrt(2*d/self.XYZ_ACCEL_FAST)
-            tm = 0
+		tr = self.XYZ_VEL_FAST/self.XYZ_ACCEL_FAST
+		tm = d/self.XYZ_VEL_FAST - tr
+		if tm < 0:
+			tr = math.sqrt(2*d/self.XYZ_ACCEL_FAST)
+			tm = 0
 
-        t_tot = tm + 2*tr
+		t_tot = tm + 2*tr
 
-        t_ = [i*1./self.rate for i in range(0,int(t_tot*self.rate))]
+		t_ = [i*1./self.rate for i in range(0,int(t_tot*self.rate))]
 
-        trajectory_msg = DeltaTrajectory()
-        joint_traj = []
-        gripper_traj = []
+		trajectory_msg = DeltaTrajectory()
+		joint_traj = []
+		gripper_traj = []
 
-        for t in t_:
-            j = JointPosition()
-            s = 0
-            if t < tr:
-                s = 0.5*self.XYZ_ACCEL_FAST*t**2
-            elif t < tr+tm:
-                s = 0.5*self.XYZ_ACCEL_FAST*tr**2 + (t - tr)*self.XYZ_VEL_FAST
-            else:
-                s = 0.5*self.XYZ_ACCEL_FAST*tr**2 + tm*self.XYZ_VEL_FAST + 0.5*self.XYZ_ACCEL_FAST*(tr**2 - (t - tm - 2*tr)**2)
-            #Interpolates directly from A to B, no hopping motion
-            xyz_i = xyz_initial + diff*s/d if d != 0 else xyz_initial
-            if self.wschecker.check(xyz_i):
-                # DO IK
-                j.angles=self.iksolver.solve(xyz_i)
-            else: 
-                # print error 
-                print("error not in workspace")
-                return
-            g = GripperPosition()
-            g.open = gripper_open
-            g.angle = orientation
-            
-            joint_traj.append(j)
-            gripper_traj.append(g)
+		for t in t_:
+			j = JointPosition()
+			s = 0
+			if t < tr:
+				s = 0.5*self.XYZ_ACCEL_FAST*t**2
+			elif t < tr+tm:
+				s = 0.5*self.XYZ_ACCEL_FAST*tr**2 + (t - tr)*self.XYZ_VEL_FAST
+			else:
+				s = 0.5*self.XYZ_ACCEL_FAST*tr**2 + tm*self.XYZ_VEL_FAST + 0.5*self.XYZ_ACCEL_FAST*(tr**2 - (t - tm - 2*tr)**2)
+			#Interpolates directly from A to B, no hopping motion
+			xyz_i = xyz_initial + diff*s/d if d != 0 else xyz_initial
+			if self.wschecker.check(xyz_i):
+				# DO IK
+				j.angles=self.iksolver.solve(xyz_i)
+			else: 
+				# print error 
+				print("error not in workspace")
+				return
+			g = GripperPosition()
+			g.open = gripper_open
+			g.angle = orientation
+			
+			joint_traj.append(j)
+			gripper_traj.append(g)
 
-        if len(joint_traj) == 0 and len(gripper_traj) == 0:
-            j = JointPosition()
-            if self.wschecker.check(xyz_initial):
-                # DO IK
-                j.angles=self.iksolver.solve(xyz_initial)
-            else: 
-                # print error 
-                print("error not in workspace")
-                return
-            g = GripperPosition()
-            g.open = gripper_open
-            g.angle = orientation
-            joint_traj.append(j)
-            gripper_traj.append(g)
+		if len(joint_traj) == 0 and len(gripper_traj) == 0:
+			j = JointPosition()
+			if self.wschecker.check(xyz_initial):
+				# DO IK
+				j.angles=self.iksolver.solve(xyz_initial)
+			else: 
+				# print error 
+				print("error not in workspace")
+				return
+			g = GripperPosition()
+			g.open = gripper_open
+			g.angle = orientation
+			joint_traj.append(j)
+			gripper_traj.append(g)
 
-        trajectory_msg.joint_trajectory = joint_traj
-        trajectory_msg.gripper_trajectory = gripper_traj
+		trajectory_msg.joint_trajectory = joint_traj
+		trajectory_msg.gripper_trajectory = gripper_traj
 
-        self.running_trajectory = True
-        self.xyz_goal_pos = list(desired_xyz)
-        self.trajectory_pub.publish(trajectory_msg)
+		self.running_trajectory = True
+		self.xyz_goal_pos = list(desired_xyz)
+		self.trajectory_pub.publish(trajectory_msg)
 
 	def generateMoveTo(self, xyz, orientation, gripper_open):
 		if self.running_trajectory: